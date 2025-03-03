da0cae3636ae86c62b0b319fc0c689c829ac780d
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-03 08:06:48 +0300

    .

--- src/main/java/com/tugalsan/api/clone/client/TGS_Cloneable.java
+++ src/main/java/com/tugalsan/api/clone/client/TGS_Cloneable.java
@@ -1,6 +1,6 @@
 package com.tugalsan.api.clone.client;
 
-abstract public class TGS_Cloneable {
+public interface TGS_Cloneable {
 
     abstract public TGS_Cloneable cloneIt();
 }
--- src/main/java/com/tugalsan/api/clone/client/TGS_CloneableUtils.java
+++ src/main/java/com/tugalsan/api/clone/client/TGS_CloneableUtils.java
@@ -5,6 +5,10 @@ import java.util.stream.*;
 
 public class TGS_CloneableUtils {
 
+    private TGS_CloneableUtils() {
+
+    }
+
     public static List<TGS_Cloneable> cloneIt(List<TGS_Cloneable> rowData) {
         return rowData.stream().collect(Collectors.toCollection(ArrayList::new));
     }
